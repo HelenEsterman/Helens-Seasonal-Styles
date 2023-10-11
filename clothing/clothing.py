58b6cb27780bc28d1428d9f4fe1ee23802fb5eb6
Helen Esterman <helenesterman99@gmail.com>
2023-10-11 12:12:39 -0400

    added base file structure and started creating clothing class

---
+++ clothing/clothing.py
@@ -0,0 +1,6 @@
+class Clothing:
+    def __init__(self, name, color, size, description):
+        self.name = name
+        self.color = color
+        self.size = size
+        self.description = description
